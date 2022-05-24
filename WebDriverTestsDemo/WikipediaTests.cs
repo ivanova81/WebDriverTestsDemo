4ccf071788ee6e3f1b35859ff5fd8bede66d5ee5
Monica Ivanova <98054187+ivanova81@users.noreply.github.com>
2022-05-24 20:07:44 +0300

    Add project files

---
+++ WebDriverTestsDemo/WikipediaTests.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+
+var driver = new ChromeDriver();
+
+driver.Url = "https://wikipedia.org";
+
+System.Console.WriteLine("CURRENT TITLE: " + driver.Title);
+
+var searchField = driver.FindElement(By.Id("searchInput"));
+searchField.Click();
+searchField.SendKeys("Quality Assurance" + Keys.Enter);
+
+System.Console.WriteLine("TITLE AFTER SEARCH: " + driver.Title);
+
+driver.Quit();
